63f17bbda2c0bbbff4b7ba9c1221ba8c3a66aebc
sidonie <shelliednnguyen@gmail.com>
2021-02-09 16:22:29 -0800

    generate html file

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-let profileDataArgs = process.argv.slice( 2, process.argv.length );
-console.log( profileDataArgs );
+const fs = require( "fs" );
+const generatePage = require( "./src/page-template.js" );
+let profileDataArgs = process.argv.slice( 2 );
+const [ name, github ] = profileDataArgs;
 
+/*
 const printProfileData = profileDataArr => {
    // This ...
    for ( let i = 0; i < profileDataArr.length; i++ ){
@@ -21,3 +24,27 @@ const printProfileData = profileDataArr => {
 };
 
 printProfileData( profileDataArgs );
+*/
+
+//const generatePage = () => "Name: Shellie, Github: shellienguyen";
+//console.log( generatePage() );
+//const generatePage = ( userName, githubName ) => `Name: ${userName}, Github: ${githubName}`;
+
+/* 
+const generatePage = (userName, githubName ) => {
+   return `
+      Name: ${userName}
+      GitHub: ${githubName}
+   `;
+};
+ */
+/* 
+console.log( name, github );
+console.log( generatePage( name, github ));
+ */
+
+ fs.writeFile( "index.html", generatePage( name, github ), err => {
+   if ( err ) throw new Error( err );
+
+   console.log( "Portfolio complete! Check out index.html to see the output!" );
+ });
