bea0ff58acb203084bdfa09ce730871bfc029f24
sidonie <shelliednnguyen@gmail.com>
2021-02-09 23:44:14 -0800

    added questions and question array

--- app.js
+++ app.js
@@ -48,15 +48,89 @@ console.log( name, github );
 console.log( generatePage( name, github ));
  */
 
- inquirer
-   .prompt([
+const promptUser = () => {
+   return inquirer.prompt([
       {
          type: "input",
          name: "name",
          message: "What is your name?"
+      },
+      {
+         type: "input",
+         name: "github",
+         message: "Enter your Github Username:"
+      },
+      {
+         type: "input",
+         name: "about",
+         message: "Provide some information about yourself:"
+      }
+   ]);
+};
+
+const promptProject = portfolioData => {
+   console.log( `
+=================
+Add a New Project
+=================
+`);
+   // If there is not a "projects" array property, create on
+   if ( !portfolioData.projects ) {
+      portfolioData.projects = [];
+   };
+
+   return inquirer.prompt([
+      {
+         type: "input",
+         name: "name",
+         message: "What is the name of your project?"
+      },
+      {
+         type: "input",
+         name: "description",
+         message: "Provide a description of the project (Required):"
+      },
+      {
+         type: "checkbox",
+         name: "languages",
+         message: "What did you build this project with? (Check all that apply):",
+         choices: [ "JavaScript", "HTML", "CSS", "ES6", "jQuery", "Bootstrap", "Node"]
+      },
+      {
+         type: "input",
+         name: "link",
+         message: "Enter the GitHub link to your project. (Required):"
+      },
+      {
+         type: "confirm",
+         name: "feature",
+         message: "Would you like to feature this project?",
+         default: false
+      },
+      {
+         type: "confirm",
+         name: "confirmAddProject",
+         message: "Would you like to enter another project?",
+         default: false
       }
    ])
-   .then( answers => console.log( answers ));
+      .then( projectData => {
+         portfolioData.projects.push( projectData );
+
+         if ( projectData.confirmAddProject ) {
+            return promptProject( portfolioData );
+         }
+         else {
+            return portfolioData;
+         };
+      })
+};
+
+promptUser()
+   .then( promptProject )
+   .then( portfolioData => {
+      console.log( portfolioData );
+   });
 
 /* 
  fs.writeFile( ".index.html", pageHTML, err => {
