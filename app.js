1e9885abb0fb680c8e6b948ea63d76947db1363e
sidonie <shelliednnguyen@gmail.com>
2021-02-14 12:50:46 -0800

    finished site

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const inquirer = require( "inquirer" );
-const fs = require( "fs" );
 const generatePage = require( "./src/page-template.js" );
+const { writeFile, copyFile } = require( "./utils/generate-site.js" );
+//const { COPYFILE_FICLONE_FORCE } = require("constants");
 //const pageHTML = generatePage( name, github );
 
 //let profileDataArgs = process.argv.slice( 2 );
@@ -194,15 +195,48 @@ const mockData = {
 const pageHTML = generatePage( mockData );
  */
 
+/* 
 promptUser()
    .then( promptProject )
    .then( portfolioData => {
       console.log( portfolioData );
       const pageHTML = generatePage( portfolioData );
       
-      fs.writeFile( ".index.html", pageHTML, err => {
-         if ( err ) throw new Error( err );
+      fs.writeFile( "./dist/index.html", pageHTML, err => {
+         if ( err ) {
+            console.log( err );
+            return;
+         };
 
-      //   console.log( "Portfolio complete! Check out index.html to see the output!" );
+         console.log( "Page created! Check out index.html in this directory to see the output!" );
+         
+         fs.copyFile( "./src/style.css", "./dist/style.css", err => {
+            if ( err ) {
+               console.log( err );
+               return;
+            };
+
+            console.log( "Style sheet copied successfully!" );
+         });
       });
 });
+ */
+
+ promptUser()
+   .then( promptProject )
+   .then( portfolioData => {
+      return generatePage( portfolioData );
+   })
+   .then( pageHTML => {
+      return writeFile( pageHTML );
+   })
+   .then( writeFileResponse => {
+      console.log( writeFileResponse );
+      return copyFile();
+   })
+   .then( copyFileResponse => {
+      console.log( copyFileResponse );
+   })
+   .catch( err => {
+      console.log( err );
+   });
